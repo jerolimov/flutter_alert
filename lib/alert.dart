32ab89b2999ab2ace038d17f6236905a35d2d956
Christoph Jerolimov <jerolimov+git+2018@gmail.com>
2018-12-16 15:05:12 +0100

    Code cleanup

--- lib/alert.dart
+++ lib/alert.dart
@@ -10,7 +10,6 @@ import 'package:flutter/material.dart';
 
 class AlertAction {
   final String text;
-  final bool enabled;
   final bool isDefaultAction;
   final bool isDestructiveAction;
   final VoidCallback onPressed;
@@ -18,7 +17,6 @@ class AlertAction {
 
   const AlertAction({
     @required this.text,
-    bool this.enabled = true,
     bool this.isDefaultAction = false,
     bool this.isDestructiveAction = false,
     @required this.onPressed,
@@ -40,79 +38,102 @@ Widget _buildBody(String body) {
   return SingleChildScrollView(child: Text(body));
 }
 
-List<Widget> _buildActions(
-    BuildContext context, bool isIOS, List<AlertAction> actions) {
-  return actions.map((AlertAction action) {
-    VoidCallback onPressed = action.enabled && action.onPressed != null
-        ? () {
-            action.onPressed();
-            if (action.automaticallyPopNavigation) {
-              NavigatorState navigatorState = Navigator.of(context);
-              if (navigatorState.canPop()) {
-                navigatorState.pop();
-              }
-            }
-          }
-        : null;
-
-    if (isIOS) {
-      return CupertinoDialogAction(
-        child: Text(
-          action.text != null ? action.text : "",
-          style: action.isDefaultAction
-              ? TextStyle(fontWeight: FontWeight.bold)
-              : null,
-        ),
-        isDefaultAction: action.isDefaultAction,
-        isDestructiveAction: action.isDestructiveAction,
-        onPressed: onPressed,
-      );
-    } else {
-      return FlatButton(
-        child: Text(
-          action.text != null ? action.text.toUpperCase() : "",
-          style: action.isDefaultAction
-              ? TextStyle(fontWeight: FontWeight.bold)
-              : null,
-        ),
-        textColor: action.isDestructiveAction ? Colors.red.shade600 : null,
-        highlightColor: action.isDestructiveAction ? Colors.red.shade50 : null,
-        onPressed: onPressed,
-      );
+Widget _buildActionButton(
+    BuildContext context, AlertAction action, bool useCupertino) {
+  VoidCallback onPressed = () {
+    if (action.automaticallyPopNavigation) {
+      NavigatorState navigatorState = Navigator.of(context);
+      if (navigatorState.canPop()) {
+        navigatorState.pop();
+      }
     }
-  }).toList();
+    if (action.onPressed != null) {
+      action.onPressed();
+    }
+  };
+
+  if (useCupertino) {
+    return CupertinoDialogAction(
+      child: Text(
+        action.text != null ? action.text : "",
+        style: action.isDefaultAction
+            ? TextStyle(fontWeight: FontWeight.bold)
+            : null,
+      ),
+      isDefaultAction: action.isDefaultAction,
+      isDestructiveAction: action.isDestructiveAction,
+      onPressed: onPressed,
+    );
+  } else {
+    return FlatButton(
+      child: Text(
+        action.text != null ? action.text.toUpperCase() : "",
+        style: action.isDefaultAction
+            ? TextStyle(fontWeight: FontWeight.bold)
+            : null,
+      ),
+      textColor: action.isDestructiveAction ? Colors.red.shade600 : null,
+      highlightColor: action.isDestructiveAction ? Colors.red.shade50 : null,
+      onPressed: onPressed,
+    );
+  }
 }
 
-showAlert({
+List<Widget> _buildActionButtons(
+  BuildContext context,
+  List<AlertAction> actions,
+  bool useCupertino,
+) {
+  return actions
+      .map((AlertAction action) =>
+          _buildActionButton(context, action, useCupertino))
+      .toList();
+}
+
+Widget _buildDialog(
+  BuildContext context,
+  String title,
+  String body,
+  List<AlertAction> actions,
+  bool useCupertino,
+) {
+  if (useCupertino) {
+    return CupertinoAlertDialog(
+      title: _buildTitle(title),
+      content: _buildBody(body),
+      actions: _buildActionButtons(context, actions, useCupertino),
+    );
+  } else {
+    return AlertDialog(
+      title: _buildTitle(title),
+      content: _buildBody(body),
+      actions: _buildActionButtons(context, actions, useCupertino),
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(5))),
+    );
+  }
+}
+
+void showAlert({
   @required BuildContext context,
   String title,
   String body,
   List<AlertAction> actions,
   bool barrierDismissible = false,
+  bool useCupertino,
 }) {
-  const isIOS = true; // Platform.isIOS
-
   if (actions == null || actions.isEmpty) {
     actions = [AlertAction(text: "OK", onPressed: () {})];
   }
 
-  Widget dialogWidget = isIOS
-      ? CupertinoAlertDialog(
-          title: _buildTitle(title),
-          content: _buildBody(body),
-          actions: _buildActions(context, true, actions),
-        )
-      : AlertDialog(
-          title: _buildTitle(title),
-          content: _buildBody(body),
-          actions: _buildActions(context, false, actions),
-          shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.all(Radius.circular(5))),
-        );
+  if (useCupertino == null) {
+    useCupertino = Platform.isIOS;
+  }
 
   showDialog(
     context: context,
     barrierDismissible: barrierDismissible,
-    builder: (BuildContext context) => dialogWidget,
+    builder: (BuildContext context) =>
+        _buildDialog(context, title, body, actions, useCupertino),
   );
 }
